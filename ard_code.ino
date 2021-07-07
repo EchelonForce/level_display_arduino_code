d725b6f591eea47245cfb39af96aecca1c2d4a2d
EchelonForce <jbookout@gmail.com>
2021-07-08 01:05:12 +0200

    ~10kHz with Peak dot

--- ard_code.ino
+++ ard_code.ino
@@ -2,7 +2,7 @@
 static byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A4}; //, A5};
                                                                                    // PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,PC4,PC5
 #include <avr/io.h>
-static const int thresholds[] = {1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 300, 400, 500, 600, 900};
+static const int thresholds[] = {1, 2, 4, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 320, 384, 448, 500};
 
 #define PIN_CNT 17
 void setup()
@@ -30,6 +30,7 @@ void setup()
 static unsigned long a = 0;
 static byte time_toggle = HIGH;
 static unsigned int sample = 0;
+static unsigned int peak = 0;
 void loop()
 {
   // digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
@@ -37,8 +38,6 @@ void loop()
   // digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
   //delay(500); // wait for a second
 
-  digitalWrite(A5, time_toggle);
-  time_toggle = !time_toggle;
   //~120us per read, standard setup
   //~34us per read, FASTADC=1
   sample = analogRead(A6);
@@ -53,9 +52,13 @@ void loop()
   {
     sample = 512 - sample;
   }
+  peak = peak_check(sample, peak);
   //a=convert(sample);//50-100us
-  a = convert2(sample); //20-40us
+  //a = convert2(sample); //20-40us
+  a = convert3(sample, peak); //50us
 
+  digitalWrite(A5, time_toggle);
+  time_toggle = !time_toggle;
   // //test all
   //   for (byte i = 0; i < PIN_CNT; i++)
   //   {
@@ -77,6 +80,25 @@ void loop()
   // }
   // digitalWrite(A5, time_toggle);
   // time_toggle = !time_toggle;
+}
+
+static byte skips = 0;
+int peak_check(int sample, int peak)
+{
+  if (sample >= peak)
+  {
+    skips = 0;
+    return sample;
+  }
+  else if (skips == 50)
+  {
+    skips = 0;
+    return peak - 1;
+  }
+  else
+  {
+    skips++;
+  }
 }
 
 long convert(int a)
@@ -123,6 +145,26 @@ long convert2(int a)
     if (a < thresholds[i])
     {
       b = (1 << i) - 1;
+      break;
+    }
+  }
+  return b;
+}
+
+long convert3(int sample, int peak)
+{
+  long b = 0;
+  int8_t idx_peak = 0;
+  for (int8_t i = PIN_CNT - 1; i >= 0; i--)
+  {
+    if (peak > thresholds[i] && idx_peak == 0)
+    {
+      idx_peak = i;
+    }
+
+    if (sample > thresholds[i])
+    {
+      b = ((1 << i) - 1) | (1 << idx_peak);
       break;
     }
   }
