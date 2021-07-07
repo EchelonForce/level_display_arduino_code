57b9a776fffef4b96bcde2390e5071688b6d82e3
EchelonForce <jbookout@gmail.com>
2021-07-07 23:34:07 +0200

    ~6kHz sample rate

--- ard_code.ino
+++ ard_code.ino
@@ -2,7 +2,7 @@
 static byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A4}; //, A5};
                                                                                    // PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,PC4,PC5
 #include <avr/io.h>
-static const int thresholds[] = {1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 300, 500, 600, 700, 900};
+static const int thresholds[] = {1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 150, 200, 300, 400, 500, 600, 900};
 
 #define PIN_CNT 17
 void setup()
@@ -17,6 +17,7 @@ void setup()
 
 static unsigned long a = 0;
 static byte time_toggle = HIGH;
+static unsigned int sample = 0;
 void loop()
 {
   // digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
@@ -24,18 +25,20 @@ void loop()
   // digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
   //delay(500); // wait for a second
 
-  a = analogRead(A6); ///100us per read
+  sample = analogRead(A6); ///100us per read
 
-  if (a >= 512)
+  digitalWrite(A5, time_toggle);
+  time_toggle = !time_toggle;
+  if (sample >= 512)
   {
-    a -= 512;
+    sample -= 512;
   }
   else
   {
-    a = 512 - a;
+    sample = 512 - sample;
   }
-
-  a = convert(a); //100 us
+  //a=convert(sample);//50-100us
+  a = convert2(sample); //20-40us
 
   digitalWrite(A5, time_toggle);
   time_toggle = !time_toggle;
@@ -46,8 +49,6 @@ void loop()
   //   }
   //display_val_bin(a); //300us
   display_val_bin2(a); //30us
-  digitalWrite(A5, time_toggle);
-  time_toggle = !time_toggle;
   // if (a == 0)
   // {
   //   a = 1;
@@ -64,7 +65,7 @@ void loop()
   // time_toggle = !time_toggle;
 }
 
-long convert(long a)
+long convert(int a)
 {
   long b = 0;
   long c = 0;
@@ -77,6 +78,38 @@ long convert(long a)
     else
     {
       c |= 1 << i;
+    }
+  }
+  return b;
+}
+
+long convert2(int a)
+{
+  // #define T1 1
+  // #define T2 10
+  // #define T3 20
+  // #define T4 30
+  // #define T5 40
+  // #define T6 50
+  // #define T7 60
+  // #define T8 70
+  // #define T9 80
+  // #define T10 90
+  // #define T11 100
+  // #define T12 110
+  // #define T13 120
+  // #define T14 130
+  // #define T15 140
+  // #define T16 150
+  // #define T17 160
+
+  long b = 0;
+  for (byte i = 0; i < PIN_CNT; i++)
+  {
+    if (a < thresholds[i])
+    {
+      b = (1 << i) - 1;
+      break;
     }
   }
   return b;
