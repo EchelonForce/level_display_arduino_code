81394531d06cf6bf896027cc3bcafd9d249bfec1
EchelonForce <jbookout@gmail.com>
2021-07-16 20:00:17 +0200

    Cleanup and reduce to 16 LEDs

--- ard_code.ino
+++ ard_code.ino
@@ -1,10 +1,10 @@
 
-static byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A4}; //, A5};
-                                                                                   // PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,PC4,PC5
+static byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3}; //, A5};
+                                                                               // PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,PC4,PC5
 #include <avr/io.h>
-static const int thresholds[] = {1, 2, 4, 8, 16, 32, 64, 96, 128, 160, 192, 224, 256, 320, 384, 448, 500};
-
-#define PIN_CNT 17
+static const int thresholds[] = {17, 21, 26, 33, 41, 52, 65, 82, 103, 129, 162, 204, 257, 324, 407, 457};
+//                              -30,-28,-26,-24,-22,-20,-18,-16, -14, -12, -10,  -8,  -6,  -4,  -2,  -1 dB  | 512=0dB
+#define PIN_CNT 16
 void setup()
 {
   for (byte i = 0; i <= PIN_CNT; i++)
@@ -12,19 +12,17 @@ void setup()
     pinMode(pins[i], OUTPUT);
   }
 #define FASTADC 1
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
 #if FASTADC
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
   // set prescale to 16
   sbi(ADCSRA, ADPS2);
   cbi(ADCSRA, ADPS1);
   cbi(ADCSRA, ADPS0);
 #endif
-  //Serial.begin(115200);
+  Serial.begin(115200);
+
+  led_test();
 }
 
 static unsigned long a = 0;
@@ -33,53 +31,13 @@ static unsigned int sample = 0;
 static unsigned int peak = 0;
 void loop()
 {
-  // digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-  // delay(250);                      // wait for a second
-  // digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  //delay(500); // wait for a second
-
-  //~120us per read, standard setup
-  //~34us per read, FASTADC=1
-  sample = analogRead(A6);
-
   digitalWrite(A5, time_toggle);
   time_toggle = !time_toggle;
-  if (sample >= 512)
-  {
-    sample -= 512;
-  }
-  else
-  {
-    sample = 512 - sample;
-  }
+
+  sample = abs(analogRead(A6) - 512);
   peak = peak_check(sample, peak);
-  //a=convert(sample);//50-100us
-  //a = convert2(sample); //20-40us
-  a = convert3(sample, peak); //50us
-
-  digitalWrite(A5, time_toggle);
-  time_toggle = !time_toggle;
-  // //test all
-  //   for (byte i = 0; i < PIN_CNT; i++)
-  //   {
-  //     a |= 1 << i;
-  //   }
-  //display_val_bin(a); //300us
-  display_val_bin2(a); //30us
-  // if (a == 0)
-  // {
-  //   a = 1;
-  // }
-  // else if (a > ((long)1 << 19))
-  // {
-  //   a = 0;
-  // }
-  // else
-  // {
-  //   a = a << 1;
-  // }
-  // digitalWrite(A5, time_toggle);
-  // time_toggle = !time_toggle;
+  a = convert(sample, peak);
+  display_val(a); //30us
 }
 
 static byte skips = 0;
@@ -101,94 +59,60 @@ int peak_check(int sample, int peak)
   }
 }
 
-long convert(int a)
+long convert(int sample, int peak) //25us
 {
   long b = 0;
-  long c = 0;
-  for (byte i = 0; i < PIN_CNT; i++)
-  {
-    if (a >= thresholds[i])
-    {
-      b |= 1 << i;
-    }
-    else
-    {
-      c |= 1 << i;
-    }
-  }
-  return b;
-}
-
-long convert2(int a)
-{
-  // #define T1 1
-  // #define T2 10
-  // #define T3 20
-  // #define T4 30
-  // #define T5 40
-  // #define T6 50
-  // #define T7 60
-  // #define T8 70
-  // #define T9 80
-  // #define T10 90
-  // #define T11 100
-  // #define T12 110
-  // #define T13 120
-  // #define T14 130
-  // #define T15 140
-  // #define T16 150
-  // #define T17 160
-
-  long b = 0;
-  for (byte i = 0; i < PIN_CNT; i++)
-  {
-    if (a < thresholds[i])
-    {
-      b = (1 << i) - 1;
-      break;
-    }
-  }
-  return b;
-}
-
-long convert3(int sample, int peak)
-{
-  long b = 0;
-  int8_t idx_peak = 0;
+  int8_t idx_peak = -1;
   for (int8_t i = PIN_CNT - 1; i >= 0; i--)
   {
-    if (peak > thresholds[i] && idx_peak == 0)
+    if (peak >= thresholds[i] && idx_peak < 0)
     {
       idx_peak = i;
     }
 
-    if (sample > thresholds[i])
+    if (sample >= thresholds[i])
     {
-      b = ((1 << i) - 1) | (1 << idx_peak);
+      b = ((1 << (i + 1)) - 1);
       break;
     }
+  }
+  if (idx_peak >= 0)
+  {
+    b |= (1 << idx_peak);
   }
   return b;
 }
 
-void display_val_bin(long a)
-{
-  for (byte i = 0; i < PIN_CNT; i++)
-  {
-    digitalWrite(pins[i], ((a >> i) & 1));
-  }
-}
-
-void display_val_bin2(long a)
+void display_val(long a)
 {
   PORTD = (PORTD & 0x03) | ((a & 0x3F) << 2);
   PORTB = (PORTB & 0xC0) | ((a & 0xFC0) >> 6);
-  PORTC = (PORTC & 0xE0) | ((a & 0x1F000) >> 12);
-  // for (byte i = 0; i < PIN_CNT; i++)
-  // {
-  //   digitalWrite(pins[i], ((a >> i) & 1));
-  // }
+  PORTC = (PORTC & 0xE0) | ((a & 0x0F000) >> 12);
 }
 
-//static byte pins[] = {2,  3,  4,  5,  6,  7,    8,  9, 10,   11, 12, 13, A0, A1, A2, A3, A4}; //, A5};
-// PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,PC4,PC5
+void led_test(void)
+{
+  //test all
+  int a = 0;
+  for (int8_t i = 0; i < PIN_CNT; i++)
+  {
+    a = convert(thresholds[i] + 1, thresholds[i] + 1);
+    display_val(a);
+    delay(100);
+  }
+
+  for (int8_t i = PIN_CNT - 1; i >= 0; i--)
+  {
+    a = convert(0, thresholds[i] + 1);
+    display_val(a);
+    delay(100);
+  }
+
+  a = 0xFFFF;
+  display_val(a);
+  delay(100);
+
+  a = 0x0;
+  display_val(a);
+  delay(100);
+}
