447aff9273bf3b0f8fcbc4c6da962dbdb76a287e
EchelonForce <jbookout@gmail.com>
2021-07-28 17:13:51 +0200

    Cleanup

--- ard_code.ino
+++ ard_code.ino
@@ -1,33 +1,45 @@
 #include <avr/io.h>
 
-static byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3}; // and A5 for timing see setup()
-//                    PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,  PC5
-static const int thresholds[] = {5, 6, 7, 9, 11, 13, 17, 21, 26, 33, 41, 51, 65, 81, 102, 115};
-//                             -30,-28,-26,-24,-22,-20,-18,-16,-14,-12,-10,-8,-6, -4, -2, -1 dB  | 127=0dB
+// Change this to speed up/slow down the initialization tests.
+#define TIME_BETWEEN_TEST_STEPS 100
+// Change this to 1 if the display is upside down, otherwise set to 0.
+#define UPSIDE_DOWN 1
+//Time between LED updates, samples are still gathered [ms]
+#define LED_UPDATE_PERIOD 1
+// How many samples to allow before decreasing the saved peak value. Decrease for faster peak update.
+#define SAMPLES_PER_PEAK_DECREMENT 1000
 
+// Number of digitial pins connected to LEDs. Rework required if changed.
 #define PIN_CNT 16
+
 void setup()
 {
+  static const byte pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A5}; // A5 for timing see below
+  //PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,  PC5
+
   for (byte i = 0; i <= PIN_CNT; i++)
   {
     pinMode(pins[i], OUTPUT);
   }
 
-  pinMode(A5, OUTPUT);    //for timing
-  PORTC = (PORTC | 0x20); //timing
-
+  PORTC = (PORTC & ~0x20); //set timing pin 0
   Serial.begin(115200);
 
   led_test();
   adc_setup();
 }
 
+/**
+ * Setup the ADC hardware for continuous measurement with an interrupt.
+ */
 void adc_setup(void)
 {
-  ADCSRA = 0;            // clear ADCSRA register
-  ADCSRB = 0;            // clear ADCSRB register
-  ADMUX |= (6 & 0x07);   // set A6 analog input pin
+  ADCSRA = 0;
+  ADCSRB = 0;
+  ADMUX |= (6 & 0x07);   // Start conversion with A6 analog input pin
   ADMUX |= (1 << REFS0); // set reference voltage
+  // The prsecaler chosen below limits the ADC to less than 10 bits.
+  // We'll use 8bits, so align the ADC so we can read that in a single instruction.
   ADMUX |= (1 << ADLAR); // left align ADC value to 8 bits from ADCH register
 
   // sampling rate is [ADC clock] / [prescaler] / [conversion clock cycles]
@@ -39,10 +51,12 @@ void adc_setup(void)
   ADCSRA |= (1 << ADSC);                 // start ADC measurements
 }
 
+//These vars used in ADC ISR and loop().
 static byte latest_sample = 0;
 static byte numSamples = 0;
-static byte time_toggle = HIGH;
-
+/**
+ * Interrupt handler for the ADC.
+ */
 ISR(ADC_vect)
 {
   ADMUX = ADMUX ^ 0x01; //swap channels 6/7 every time, seems to limit signal for some reason
@@ -57,109 +71,126 @@ ISR(ADC_vect)
   numSamples++;
 }
 
-static unsigned long a = 0;
-static int sample = 0;
 static unsigned int peak = 0;
-static unsigned int sample_peaks = 0;
-static int t_last = 0;
-
+static unsigned int max_sample_between_led_updates = 0;
+static int t_last_led_update = 0;
 void loop()
 {
   if (numSamples > 0)
   {
     numSamples = 0;
-    sample = latest_sample;
+    int sample = latest_sample;
     sample = abs(sample - 127);
-    sample_peaks = max(sample, sample_peaks);
-    peak = peak_check(sample_peaks, peak);
+    max_sample_between_led_updates = max(sample, max_sample_between_led_updates);
+    peak = peak_update(max_sample_between_led_updates, peak);
 
+    //update the leds
     int t_now = millis();
-    if (t_now - t_last > 0)
+    if (t_now - t_last_led_update >= LED_UPDATE_PERIOD)
     {
-      t_last = t_now;
-      PORTC = (PORTC | 0x20); //timing
-      a = convert2(sample_peaks, peak);
-      PORTC = (PORTC ^ 0x20); //timing
+      t_last_led_update = t_now;
+      //PORTC = (PORTC | 0x20); //timing
+      unsigned long a = convert(max_sample_between_led_updates, peak);
+      //PORTC = (PORTC ^ 0x20); //timing
       display_val(a);
-      sample_peaks = 0;
+      max_sample_between_led_updates = 0;
     }
   }
 }
 
-static int skips = 0;
-int peak_check(int sample, int peak)
+static int peak_update_skips = 0;
+/**
+ * Increases the peak if sample is larger than the current peak. Reduces peak by 1 if the
+ * sample has been lower than the peak for SAMPLES_PER_PEAK_DECREMENT samples
+ * (if peak hasn't changed).
+ */
+int peak_update(int sample, int peak) //2us
 {
   if (sample >= peak)
   {
-    skips = 0;
+    peak_update_skips = 0;
     return sample;
   }
-  else if (skips == 1000) //tweak to make the peak last longer.
+  else if (peak_update_skips == SAMPLES_PER_PEAK_DECREMENT) //tweak to make the peak last longer.
   {
-    skips = 0;
+    peak_update_skips = 0;
     return peak - 1;
   }
   else
   {
-    skips++;
+    peak_update_skips++;
+    return peak;
   }
 }
 
-uint16_t convert(int sample, int peak) //23us w/o reverse, 85 w/ reverse, 29 w/ reverse 2
+/**
+ * Convert the latest sample and peak into a 16bit value that is later used to update LEDs.
+ * Uses two look up tables that coorespond to thresholds of dB see lut_gen.py
+ */
+uint16_t convert(int sample, int peak) //3us w/o reverse, 8us w/ reverse
 {
-  if (sample >= thresholds[PIN_CNT - 1])
-  {
-    return 0xFFFF;
-  }
-  uint16_t b = 0;
-  int8_t idx_peak = (peak >= thresholds[PIN_CNT - 1]) ? PIN_CNT - 1 : -1;
-  for (int8_t i = PIN_CNT - 2; i >= 0; i--)
-  {
-    if (peak >= thresholds[i] && idx_peak < 0)
-    {
-      idx_peak = i;
-    }
-
-    if (sample >= thresholds[i])
-    {
-      b = ((1 << (i + 1)) - 1);
-      break;
-    }
-  }
-  if (idx_peak >= 0)
-  {
-    b |= (1 << idx_peak);
-  }
-  //return (b);
-  return reverse_bits_2(b); //use if upsidedown.
-}
-
-uint16_t convert2(int sample, int peak) //3us w/o reverse, 8us w/ reverse
-{
-  static const uint16_t lookup_sample[] = {0, 0, 0, 0, 0, 0x1, 0x3, 0x7, 0x7, 0xf, 0xf, 0x1f, 0x1f, 0x3f, 0x3f, 0x3f, 0x3f, 0x7f, 0x7f, 0x7f, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff};
-  static const uint16_t lookup_peak[] = {0, 0, 0, 0, 0, 0x1, 0x2, 0x4, 0x4, 0x8, 0x8, 0x10, 0x10, 0x20, 0x20, 0x20, 0x20, 0x40, 0x40, 0x40, 0x40, 0x80, 0x80, 0x80, 0x80, 0x80, 0x100, 0x100, 0x100, 0x100, 0x100, 0x100, 0x100, 0x200, 0x200, 0x200, 0x200, 0x200, 0x200, 0x200, 0x200, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000};
+  static const uint16_t lookup_sample[] = {
+      0, 0, 0, 0, 0,                                                                                                                                                          // 0-4
+      0x1,                                                                                                                                                                    // 5-5
+      0x3,                                                                                                                                                                    // 6-6
+      0x7, 0x7,                                                                                                                                                               // 7-8
+      0xf, 0xf,                                                                                                                                                               // 9-10
+      0x1f, 0x1f,                                                                                                                                                             // 11-12
+      0x3f, 0x3f, 0x3f, 0x3f,                                                                                                                                                 // 13-16
+      0x7f, 0x7f, 0x7f, 0x7f,                                                                                                                                                 // 17-20
+      0xff, 0xff, 0xff, 0xff, 0xff,                                                                                                                                           // 21-25
+      0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff, 0x1ff,                                                                                                                        // 26-32
+      0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff, 0x3ff,                                                                                                                 // 33-40
+      0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff, 0x7ff,                                                                                                   // 41-50
+      0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff,                                                                       // 51-64
+      0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff, 0x1fff,                                         // 65-80
+      0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, 0x3fff, // 81-101
+      0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff, 0x7fff,                                                                 // 102-114
+      0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff, 0xffff                                                                  // 115-126
+  };
+  static const uint16_t lookup_peak[] = {
+      0, 0, 0, 0, 0,                                                                                                                                                          // 0-4
+      0x1,                                                                                                                                                                    // 5-5
+      0x2,                                                                                                                                                                    // 6-6
+      0x4, 0x4,                                                                                                                                                               // 7-8
+      0x8, 0x8,                                                                                                                                                               // 9-10
+      0x10, 0x10,                                                                                                                                                             // 11-12
+      0x20, 0x20, 0x20, 0x20,                                                                                                                                                 // 13-16
+      0x40, 0x40, 0x40, 0x40,                                                                                                                                                 // 17-20
+      0x80, 0x80, 0x80, 0x80, 0x80,                                                                                                                                           // 21-25
+      0x100, 0x100, 0x100, 0x100, 0x100, 0x100, 0x100,                                                                                                                        // 26-32
+      0x200, 0x200, 0x200, 0x200, 0x200, 0x200, 0x200, 0x200,                                                                                                                 // 33-40
+      0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400, 0x400,                                                                                                   // 41-50
+      0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800, 0x800,                                                                       // 51-64
+      0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000, 0x1000,                                         // 65-80
+      0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, 0x2000, // 81-101
+      0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000, 0x4000,                                                                 // 102-114
+      0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000, 0x8000                                                                  // 115-126
+  };
+#if UPSIDE_DOWN == 0
   return lookup_sample[min(127, sample)] | lookup_peak[min(127, peak)];
-  //return reverse_bits_2(lookup_sample[min(127, sample)] | lookup_peak[min(127, peak)]); //use if upsidedown.
+#else
+  return reverse_bits(lookup_sample[min(127, sample)] | lookup_peak[min(127, peak)]);
+#endif
 }
 
-uint16_t reverse_bits(uint16_t a) //62 us
-{
-  uint16_t b = 0;
-  for (byte i = 0; i < PIN_CNT; i++)
-  {
-    b = b << 1 | ((a >> i) & 1);
-  }
-  return b;
-}
-
-uint16_t reverse_bits_2(uint16_t b) //6us
+#if UPSIDE_DOWN != 0
+/**
+ * Reverses a 16 bit value's bits.
+ */
+uint16_t reverse_bits(uint16_t b) //5us
 {
   b = ((b & 0x5555) << 1) | ((b & 0xAAAA) >> 1);
   b = ((b & 0x3333) << 2) | ((b & 0xCCCC) >> 2);
   b = ((b & 0x0F0F) << 4) | ((b & 0xF0F0) >> 4);
   return ((b & 0x00FF) << 8) | ((b & 0xFF00) >> 8);
 }
+#endif
 
+/**
+ * Map a 16 bit value to the LEDs.
+ * Very specific to the Arduino Pro Mini, optimized for fast digital output.
+ */
 void display_val(uint16_t a) //4us
 {
   PORTD = (PORTD & 0x03) | ((a & 0x3F) << 2);
@@ -167,16 +198,21 @@ void display_val(uint16_t a) //4us
   PORTC = (PORTC & 0xF0) | ((a & 0xF000) >> 12);
 }
 
+/**
+ * Test all the functions and LEDs, also makes a nice boot sequence.
+ */
 void led_test(void)
 {
-#define TIME_BETWEEN 300
+  int thresholds[] = {5, 6, 7, 9, 11, 13, 17, 21, 26, 33, 41, 51, 65, 81, 102, 115};
+  //                -30,-28,-26,-24,-22,-20,-18,-16,-14,-12,-10,-8,-6, -4, -2, -1 dB  | 127=0dB
+
   //test all the thresholds
   int a = 0;
   for (int8_t i = 0; i < PIN_CNT; i++)
   {
     a = convert(thresholds[i], thresholds[i]);
     display_val(a);
-    delay(TIME_BETWEEN);
+    delay(TIME_BETWEEN_TEST_STEPS);
   }
 
   //test peaks in reverse
@@ -184,16 +220,16 @@ void led_test(void)
   {
     a = convert(0, thresholds[i]);
     display_val(a);
-    delay(TIME_BETWEEN);
+    delay(TIME_BETWEEN_TEST_STEPS);
   }
 
   //all on
   a = 0xFFFF;
   display_val(a);
-  delay(TIME_BETWEEN);
+  delay(TIME_BETWEEN_TEST_STEPS);
 
   //all off
   a = 0;
   display_val(a);
-  delay(TIME_BETWEEN);
+  delay(TIME_BETWEEN_TEST_STEPS);
 }
--- lut_gen.py
+++ lut_gen.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+
+# Generates the two look up tables needed for fast LED updates.
 num_leds = 16
 
 # 8 bit ADC, but the signal is centered and then rectified to half range.
 max_sens = 128
 
 thresholds = [5, 6, 7, 9, 11, 13, 17, 21, 26, 33, 41, 51, 65, 81, 102, 115]
+#            -30,-28,-26,-24,-22,-20,-18,-16,-14,-12,-10,-8,-6, -4, -2, -1 dB  | 127=0dB
 sense_led = [(1 << (i + 1)) - 1 for i in range(num_leds)]
 peak_led = [1 << i for i in range(num_leds)]
 vals = [i for i in range(max_sens)]
@@ -14,9 +18,13 @@ vals = [i for i in range(max_sens)]
 
 out_lut_sense = ""
 out_lut_peak = ""
+
+prev_out_sense = ""
+prev_val = 0
 for val in vals:
     out_sense = "0,"
     out_peak = "0,"
+    thresh_comment = ""
     for idx, threshold in enumerate(reversed(thresholds)):
         if val >= threshold:
             out_sense = hex(sense_led[num_leds - idx - 1]) + ","
@@ -27,9 +35,27 @@ for val in vals:
             #     hex(sense_led[num_leds - idx - 1]),
             #     hex(peak_led[num_leds - idx - 1]),
             # )
+            thresh_comment = " // {a}-{b}".format(a=prev_val, b=val - 1)
             break
+    # newline/comment handling
+    if prev_out_sense != out_sense:
+        prev_out_sense = out_sense
+        out_lut_sense += thresh_comment + "\n"
+        out_lut_peak += thresh_comment + "\n"
+        prev_val = val
+
     out_lut_sense += out_sense
     out_lut_peak += out_peak
 
-print(out_lut_sense)
-print(out_lut_peak)
+print(
+    "static const uint16_t lookup_sample[] = {"
+    + out_lut_sense[:-1]
+    + thresh_comment
+    + "\n};"
+)
+print(
+    "static const uint16_t lookup_peak[] = {"
+    + out_lut_peak[:-1]
+    + thresh_comment
+    + "\n};"
+)
