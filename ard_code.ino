1fb07e825541ab37f554e6ba605fa46db76d4de1
EchelonForce <jbookout@gmail.com>
2022-02-13 21:44:41 +0100

    Auto formating

--- ard_code.ino
+++ ard_code.ino
@@ -4,7 +4,7 @@
 #define TIME_BETWEEN_TEST_STEPS 100
 // Change this to 1 if the display is upside down, otherwise set to 0.
 #define UPSIDE_DOWN 0
-//Time between LED updates, samples are still gathered [ms]
+// Time between LED updates, samples are still gathered [ms]
 #define LED_UPDATE_PERIOD 1
 // How many samples to allow before decreasing the saved peak value. Decrease for faster peak update.
 #define SAMPLES_PER_PEAK_DECREMENT 1000
@@ -15,14 +15,14 @@
 void setup()
 {
   static const uint8_t pins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, A0, A1, A2, A3, A5}; // A5 for timing see below
-  //PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,  PC5
+  // PD2,PD3,PD4,PD5,PD6,PD7,  PB0,PB1,PB2,PB3,PB4,PB5  PC0,PC1,PC2,PC3,  PC5
 
   for (byte i = 0; i <= PIN_CNT; i++)
   {
     pinMode(pins[i], OUTPUT);
   }
 
-  PORTC = (PORTC & ~0x20); //set timing pin 0
+  PORTC = (PORTC & ~0x20); // set timing pin 0
   Serial.begin(115200);
 
   led_test();
@@ -51,7 +51,7 @@ void adc_setup(void)
   ADCSRA |= (1 << ADSC);                 // start ADC measurements
 }
 
-//These vars used in ADC ISR and loop().
+// These vars used in ADC ISR and loop().
 static uint8_t latest_sample = 0;
 static uint8_t numSamples = 0;
 /**
@@ -59,7 +59,7 @@ static uint8_t numSamples = 0;
  */
 ISR(ADC_vect)
 {
-  ADMUX = ADMUX ^ 0x01; //swap channels 6/7 every time, seems to limit signal for some reason
+  ADMUX = ADMUX ^ 0x01; // swap channels 6/7 every time, seems to limit signal for some reason
   if (numSamples > 0)
   {
     latest_sample = max(latest_sample, ADCH); // read 8 bit value from ADC
@@ -86,14 +86,14 @@ void loop()
     max_sample_between_led_updates = max(sample, max_sample_between_led_updates);
     peak = peak_update(max_sample_between_led_updates, peak);
 
-    //update the leds
+    // update the leds
     unsigned long t_now = millis();
     if (t_now - t_last_led_update >= LED_UPDATE_PERIOD)
     {
       t_last_led_update = t_now;
-      //PORTC = (PORTC | 0x20); //timing
+      // PORTC = (PORTC | 0x20); //timing
       unsigned long a = convert(max_sample_between_led_updates, peak);
-      //PORTC = (PORTC ^ 0x20); //timing
+      // PORTC = (PORTC ^ 0x20); //timing
       display_val(a);
       max_sample_between_led_updates = 0;
     }
@@ -106,14 +106,14 @@ static unsigned int peak_update_skips = 0;
  * sample has been lower than the peak for SAMPLES_PER_PEAK_DECREMENT samples
  * (if peak hasn't changed).
  */
-uint8_t peak_update(uint8_t sample, uint8_t peak) //2us
+uint8_t peak_update(uint8_t sample, uint8_t peak) // 2us
 {
   if (sample >= peak)
   {
     peak_update_skips = 0;
     return sample;
   }
-  else if (peak_update_skips == SAMPLES_PER_PEAK_DECREMENT) //tweak to make the peak last longer.
+  else if (peak_update_skips == SAMPLES_PER_PEAK_DECREMENT) // tweak to make the peak last longer.
   {
     peak_update_skips = 0;
     return peak - 1;
@@ -129,7 +129,7 @@ uint8_t peak_update(uint8_t sample, uint8_t peak) //2us
  * Convert the latest sample and peak into a 16bit value that is later used to update LEDs.
  * Uses two look up tables that coorespond to thresholds of dB see lut_gen.py
  */
-uint16_t convert(uint8_t sample, uint8_t peak) //3us w/o reverse, 8us w/ reverse
+uint16_t convert(uint8_t sample, uint8_t peak) // 3us w/o reverse, 8us w/ reverse
 {
   static const uint16_t lookup_sample[] = {
       0, 0, 0, 0, 0,                                                                                                                                                          // 0-4
@@ -180,7 +180,7 @@ uint16_t convert(uint8_t sample, uint8_t peak) //3us w/o reverse, 8us w/ reverse
 /**
  * Reverses a 16 bit value's bits.
  */
-uint16_t reverse_bits(uint16_t b) //5us
+uint16_t reverse_bits(uint16_t b) // 5us
 {
   b = ((b & 0x5555) << 1) | ((b & 0xAAAA) >> 1);
   b = ((b & 0x3333) << 2) | ((b & 0xCCCC) >> 2);
@@ -193,7 +193,7 @@ uint16_t reverse_bits(uint16_t b) //5us
  * Map a 16 bit value to the LEDs.
  * Very specific to the Arduino Pro Mini, optimized for fast digital output.
  */
-void display_val(uint16_t a) //4us
+void display_val(uint16_t a) // 4us
 {
   PORTD = (PORTD & 0x03) | ((a & 0x3F) << 2);
   PORTB = (PORTB & 0xC0) | ((a & 0xFC0) >> 6);
@@ -208,7 +208,7 @@ void led_test(void)
   uint8_t thresholds[] = {5, 6, 7, 9, 11, 13, 17, 21, 26, 33, 41, 51, 65, 81, 102, 115};
   //                     -30,-28,-26,-24,-22,-20,-18,-16,-14,-12,-10,-8,-6, -4, -2, -1 dB  | 127=0dB
 
-  //test all the thresholds
+  // test all the thresholds
   int a = 0;
   for (int8_t i = 0; i < PIN_CNT; i++)
   {
@@ -217,7 +217,7 @@ void led_test(void)
     delay(TIME_BETWEEN_TEST_STEPS);
   }
 
-  //test peaks in reverse
+  // test peaks in reverse
   for (int8_t i = PIN_CNT - 1; i >= 0; i--)
   {
     a = convert(0, thresholds[i]);
@@ -225,12 +225,12 @@ void led_test(void)
     delay(TIME_BETWEEN_TEST_STEPS);
   }
 
-  //all on
+  // all on
   a = 0xFFFF;
   display_val(a);
   delay(TIME_BETWEEN_TEST_STEPS);
 
-  //all off
+  // all off
   a = 0;
   display_val(a);
   delay(TIME_BETWEEN_TEST_STEPS);
